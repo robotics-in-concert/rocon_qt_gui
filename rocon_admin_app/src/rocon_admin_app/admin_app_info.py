881ea3503cc7d2f7a57511a8cd93fb01eaf290c5
Daniel Stonier <d.stonier@gmail.com>
2014-03-13 09:35:11 +0900

    ConcertServices -> Services and some pep8

--- rocon_admin_app/src/rocon_admin_app/admin_app_info.py
+++ rocon_admin_app/src/rocon_admin_app/admin_app_info.py
@@ -5,7 +5,7 @@
 #
 import rospy
 
-from concert_msgs.msg import ConcertServices
+import concert_msgs.msg as concert_msgs
 
 ##############################################################################
 # Graph
@@ -18,23 +18,21 @@ class AdminAppInfo(object):
 
         self.service_list = {}
         # should make use of concert_msgs/Strings here.
-        rospy.Subscriber("/concert/services/list", ConcertServices, self.update_service_list)
+        rospy.Subscriber("/concert/services/list", concert_msgs.Services, self.update_service_list)
 
     def _reg_event_callback(self, func):
         self._event_callback = func
 
-
-    def update_service_list(self, data):   
+    def update_service_list(self, data):
         print "update_service_list start"
         self.service_list = {}
         for k in data.services:
             service_name = k.name
-            resource_name = k.resource_name
             self.service_list[service_name] = {}
             self.service_list[service_name]['name'] = service_name
             self.service_list[service_name]['resource_name'] = k.resource_name
             self.service_list[service_name]['description'] = k.description
-            
+
             self.service_list[service_name]['author'] = k.author
             self.service_list[service_name]['priority'] = k.priority
             self.service_list[service_name]['launcher_type'] = k.launcher_type
@@ -44,24 +42,23 @@ class AdminAppInfo(object):
             self.service_list[service_name]['enabled'] = k.enabled
             # not implementation
             self.service_list[service_name]['client_list'] = {}
-                
+
             #html
             service_context = "<html>"
             service_context += "<p>-------------------------------------------</p>"
-            service_context += "<p><b>name: </b>" +k.name+"</p>"
-            service_context += "<p><b>resource_name: </b>" +k.resource_name+"</p>"
-            service_context += "<p><b>description: </b>" +k.description+"</p>"
-            service_context += "<p><b>author: </b>" +k.author+"</p>"
-            service_context += "<p><b>priority: </b>" +str(k.priority)+"</p>"
-            service_context += "<p><b>launcher_type: </b>" +k.launcher_type+"</p>"
-            service_context += "<p><b>launcher: </b>" +k.launcher+"</p>"
-            service_context += "<p><b>uuid: </b>" +str(k.uuid)+"</p>"
-            service_context += "<p><b>status: </b>" +str(k.status)+"</p>"
-            service_context += "<p><b>enabled: </b>" +str(k.enabled)+"</p>"
-            service_context +="</html>"
-            
+            service_context += "<p><b>name: </b>" + k.name + "</p>"
+            service_context += "<p><b>resource_name: </b>" + k.resource_name + "</p>"
+            service_context += "<p><b>description: </b>" + k.description + "</p>"
+            service_context += "<p><b>author: </b>" + k.author + "</p>"
+            service_context += "<p><b>priority: </b>" + str(k.priority) + "</p>"
+            service_context += "<p><b>launcher_type: </b>" + k.launcher_type + "</p>"
+            service_context += "<p><b>launcher: </b>" + k.launcher + "</p>"
+            service_context += "<p><b>uuid: </b>" + str(k.uuid) + "</p>"
+            service_context += "<p><b>status: </b>" + str(k.status) + "</p>"
+            service_context += "<p><b>enabled: </b>" + str(k.enabled) + "</p>"
+            service_context += "</html>"
+
             self.service_list[service_name]['context'] = service_context
-            
+
         if self._event_callback != None:
             self._event_callback()
-     
