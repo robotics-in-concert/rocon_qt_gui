db984423222093df4f9f5c23b5fb45cc10c60235
dwlee <dwlee@yujinrobot.com>
2014-10-12 19:00:01 +0900

    update concert_admin_app

--- concert_admin_app/setup.py
+++ concert_admin_app/setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rocon_admin_app'],
+    packages=['concert_admin_app'],
     package_dir={'': 'src'},
-    scripts=['scripts/rocon_admin_app'],
+    scripts=['scripts/concert_admin_app'],
     requires=['qt_gui_py_common', 'rqt_gui', 'rqt_gui_py', 'rospy', 'rospkg']
 )
 setup(**d)
