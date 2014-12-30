5ad2b03825b0bf9bcf53fd207874984ceb7878f3
Jihoon Lee <jihoonlee.in@gmail.com>
2014-12-30 11:16:23 +0900

    update setup.py to install submodules fixes #173

--- rocon_qt_library/setup.py
+++ rocon_qt_library/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rocon_qt_library'],
+    packages=['rocon_qt_library', 'rocon_qt_library.interfaces', 'rocon_qt_library.utils', 'rocon_qt_library.views', 'rocon_qt_library.widgets'],
     package_dir={'': 'src'},
 )
 setup(**d)
