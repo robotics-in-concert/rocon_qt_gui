160010ce1a9b0393e2411a856735d5dca8767989
Daniel Stonier <d.stonier@gmail.com>
2016-02-25 01:37:24 +0900

    [rocon_remocon] rqt_wrapper dependency moved to its own package

--- rocon_remocon/setup.py
+++ rocon_remocon/setup.py
@@ -6,7 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rocon_remocon'],
     package_dir={'': 'src'},
-    scripts=['scripts/rqt_remocon' ],
-    #scripts=['scripts/rocon_remocon','scripts/rqt_remocon' ],
+    scripts=['scripts/rqt_remocon', 'scripts/wrqt_remocon' ],
 )
 setup(**d)
